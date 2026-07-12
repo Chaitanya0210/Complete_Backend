e38a75ef7bd3982aceea1763bbf18e4a7f67f616
Chaitanya0210 <rayewarchaitanya@gmail.com>
2026-07-12 12:26:53 +0530

    registered user

--- src/app.js
+++ src/app.js
@@ -12,10 +12,19 @@ import cookieParser from 'cookie-parser'
  app.use(express.json({limit: "16kb"}))
  app.use(express.urlencoded({extended: true,limit:"16kb"}))
  app.use(express.static("public"))
- app.use(cookieParser)
+ app.use(cookieParser())
+
+
+app.get("/ping", (req, res) => {
+    res.send("pong")
+})
+
+
+import userRouter  from './routes/user.routes.js'
 
 
 
+app.use("/api/v1/users",userRouter)
 
 
 export {app}
